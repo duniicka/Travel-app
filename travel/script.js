e08f2a50ae132251044e39678a5d3ca202812244
unknown <dunyaei@code.edu.az>
2024-12-21 23:27:45 +0400

    toggle menu

---
+++ travel/script.js
@@ -0,0 +1,17 @@
+function toggleList() {
+    var x = document.getElementById("toggle-list");
+    if (x.style.display === "block") {
+      x.style.display = "none";
+    } else {
+      x.style.display = "block";
+    }
+  }
+
+function blogToggle(params) {
+    var y = document.getElementById('blog-toggle-list');
+    if (y.style.display === "block") {
+      y.style.display = "none";
+    } else {
+      y.style.display = "block";
+    }
+}
