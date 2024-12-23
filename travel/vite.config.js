349ab226d08f3e071d1ab6fd0aca3c70b6956a62
unknown <dunyaei@code.edu.az>
2024-12-23 14:04:36 +0400

    updates

--- travel/vite.config.js
+++ travel/vite.config.js
@@ -1,13 +1,14 @@
 import {defineConfig} from "vite";
+
 export default defineConfig({
     build: {
         rollupOptions: {
             input: {
                 main: "index.html",
-                about: "about.html",
                 contact: "contact.html",
-                blog: "blog.html",
+                about: "about.html",
                 singleblog: "single-blog.html",
+                blog: "blog.html",
                 destination: "destination.blog"
             }
         }
